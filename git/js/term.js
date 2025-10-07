71905d943b87008a29b946f623517ff444a6bf3d
melissa-24 <mlongenberger@codingdojo.com>
2025-10-07 19:30:42 -0400

    finished term commands

--- git/js/term.js
+++ git/js/term.js
@@ -5,7 +5,7 @@ const outputContainer = document.getElementById('output')
 const fileList = ['Documents', 'Pictures'];
 var fileLocation = '/Users/guest'
 var baseLocation = 'BeeDevHelper $~/'
-
+var step = 0
 
 function startTerminal() {
     fakeTerminal.style.display = 'flex'
@@ -19,15 +19,25 @@ function startTerminal() {
         fileList.pop()
     }
 }
-
-function getTimestamp() {
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-  return `[${hours}:${minutes}:${seconds}]`;
+function clearTerminal() {
+    fakeTerminal.style.display = 'flex'
+    commandInput.style.display = 'inline'
+    termPrompt.innerHTML = `${baseLocation}`
+    outputContainer.innerHTML = ''
+    commandInput.value = ''
+    commandInput.focus()
 }
+function finalTerm() {
+    fakeTerminal.style.display = 'flex'
+    commandInput.style.display = 'inline'
+    termPrompt.innerHTML = `All commands practiced: pwd, ls, cd, mkdir, touch`
+    outputContainer.innerHTML = ''
+    commandInput.value = ''
+    commandInput.focus()
+}
+
 function executeBasicTerm() {
+    console.log("Start Step #", step, baseLocation)
     const command = commandInput.value
     // Display the typed command in the terminal
     termPrompt.innerHTML += `<span class="user-command"> ${command}</span><br>`;
@@ -44,22 +54,89 @@ function executeBasicTerm() {
         unhideSteps(command)
     }
     else if (command === 'cd Documents') {
-        baseLocation = `${baseLocation}Documents/`
-        fileLocation = `${fileLocation}/Documents`
+        fileList.pop()
+        fileList.pop()
+        fileList.push('hello-world.html')
+        fileList.push('Project')
+        baseLocation += 'Documents/'
+        fileLocation += '/Documents'
         unhideSteps(command)
     } 
+    else if (command === 'cd ..') {
+        fileList.pop()
+        fileList.pop()
+        fileList.pop()
+        fileList.push('Documents')
+        fileList.push('Pictures')
+        if (step === 4) {
+            baseLocation = 'BeeDevHelper $~/'
+            fileLocation = '/Users/guest'
+        }
+        unhideSteps(command)
+    }
+    else if (command === 'cd ../..') {
+        if (step === 13) {
+            fileList.pop()
+            fileList.pop()
+            fileList.pop()
+            fileList.push('Documents')
+            fileList.push('Pictures')
+            baseLocation = 'BeeDevHelper $~/'
+            fileLocation = '/Users/guest'
+            unhideSteps(command)
+        }
+    }
+    else if (command === 'clear') {
+        clearTerminal()
+        unhideSteps(command)
+        commandInput.value = ''
+        commandInput.focus()
+        return
+    }
+    else if (command === 'mkdir Test') {
+        if (step === 7) {
+            fileList.push('Test')
+            unhideSteps(command)
+        } else if (baseLocation = 'BeeDevHelper $~/Documents/'){
+            outputText = `Can not ${command}\nAlready exists`
+        } else {
+            outputText = `${command} not valid at this location`
+        }
+    }
+    else if (command === 'cd Test') {
+        if (step === 9) {
+            fileList.pop()
+            fileList.pop()
+            fileList.pop()
+            fileList.pop()
+            baseLocation += 'Test/'
+            fileLocation += `/Test`
+            unhideSteps(command)
+        } else {
+            outputText = `cd: no such file or directory: Test`
+        }
+    }
+    else if (command === 'touch index.html') {
+        if (step === 11) {
+            fileList.push('index.html')
+            unhideSteps(command)
+        } else {
+            outputText = `${command} not valid at this location`
+        }
+    }
     else {
-        const outputText = `Executing: ${command}\nCommand not found!`;
+        const outputText = `Executing: Command not found ${command}`;
         termPrompt.innerHTML += `${outputText}<br>`;
         unhideSteps(command)
     }
     termPrompt.innerHTML += `${baseLocation}`
     commandInput.value = ''
     commandInput.focus()
+    console.log("End Step #", step, baseLocation)
 }
 
 var idToShow = ''
-var step = 0
+var hide = ''
 function unhideSteps(command) {
     if(command == 'pwd') {
         if(step === 0) {
@@ -78,7 +155,8 @@ function unhideSteps(command) {
             }, 2000)
             console.log(step)
         }
-    } else if (command == 'ls') {
+    } 
+    else if (command == 'ls') {
         if(step === 1) {
             idToShow = document.getElementById('step2desc')
             step = 2
@@ -95,10 +173,9 @@ function unhideSteps(command) {
             }, 2000)
             console.log(step)
         }
-    } else if ( command === 'cd Documents'){
-        if(step === 2) {
-            idToShow = document.getElementById('step3desc')
-            step = 3
+        else if (step === 3.5) {
+            idToShow = document.getElementById('step3adesc')
+            step = 4
             setTimeout(function() {
                 idToShow.style.display = 'block'
                 idToShow.style.backgroundColor = 'green'
@@ -108,6 +185,224 @@ function unhideSteps(command) {
                 setTimeout(function() {
                     idToShow = document.getElementById('step4Start')
                     idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        } 
+        else if (step == 8) {
+            idToShow = document.getElementById('step8desc')
+            step = 9
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+                
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step9Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        } 
+        else if (step === 10) {
+            idToShow = document.getElementById('step10desc')
+            step = 11
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+                
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step11Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+        else if (step === 12) {
+            idToShow = document.getElementById('step12desc')
+            step = 13
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+                
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step13Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+    } 
+    else if ( command === 'cd Documents'){
+        if(step === 2) {
+            idToShow = document.getElementById('step3desc')
+            step = 3.5
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step3aStart')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+        else if (step === 6) {
+            idToShow = document.getElementById('step6desc')
+            step = 7
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step7Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+    } 
+    else if ( command === 'cd ..' ) {
+        if (step === 4) {
+            idToShow = document.getElementById('step4desc')
+            step = 5
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step5Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+    } 
+    else if (command === 'clear') {
+        if (step === 5) {
+            idToShow = document.getElementById('step5desc')
+            hide = document.getElementById('step1Start')
+            step = 6
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+                hide.style.display = 'none'
+                hide = document.getElementById('step2Start')
+                hide.style.display = 'none'
+                hide = document.getElementById('step3Start')
+                hide.style.display = 'none'
+                hide = document.getElementById('step3aStart')
+                hide.style.display = 'none'
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step6Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+    }
+    else if (command === 'mkdir Test') {
+        if(step === 7) {
+            idToShow = document.getElementById('step7desc')
+            step = 8
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step8Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+    }
+    else if (command === 'cd Test') {
+        if (step === 9) {
+            idToShow = document.getElementById('step9desc')
+            hide = document.getElementById('step4Start')
+            step = 10
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+                hide.style.display = 'none'
+                hide = document.getElementById('step5Start')
+                hide.style.display = 'none'
+                hide = document.getElementById('step6Start')
+                hide.style.display = 'none'
+                hide = document.getElementById('step7Start')
+                hide.style.display = 'none'
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step10Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+    }
+    else if (command == 'touch index.html') {
+        if (step === 11) {
+            idToShow = document.getElementById('step11desc')
+            step = 12
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step12Start')
+                    idToShow.style.display = 'table-row'
+                }, 2000)
+            }, 2000)
+        }
+    }
+    else if (command === 'cd ../..') {
+        if(step === 13) {
+            idToShow = document.getElementById('step13desc')
+            hide = document.getElementById('step8Start')
+            step = 14
+            setTimeout(function() {
+                idToShow.style.display = 'block'
+                idToShow.style.backgroundColor = 'green'
+                idToShow.style.color = 'white'
+                idToShow.style.padding = '.5em'
+                hide.style.display = 'none'
+                hide = document.getElementById('step9Start')
+                hide.style.display = 'none'
+                hide = document.getElementById('step10Start')
+                hide.style.display = 'none'
+                hide = document.getElementById('step11Start')
+                hide.style.display = 'none'
+                hide = document.getElementById('step12Start')
+                hide.style.display = 'none'
+
+                setTimeout(function() {
+                    idToShow = document.getElementById('step14Start')
+                    idToShow.style.display = 'table-row'
+                    hide = document.getElementById('step13Start')
+                    hide.style.display = 'none'
+
+                    setTimeout(function() {
+                        clearTerminal()
+
+                        setTimeout(function() {
+                            finalTerm()
+                        }, 1000)
+                    }, 1000)
                 }, 2000)
             }, 2000)
         }
